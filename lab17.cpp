5d91e827885696b6876b66720fdb90a39aece3ca
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-11-16 18:26:20 -0500

    Initial Commit

--- lab17.cpp
+++ lab17.cpp
@@ -1,11 +1,1 @@
 // Definition of findHigh function goes here
-template < typename A, typename B, typename C, typename D>
-void findHigh(A data[], B size, C &index, D &highVal){
-    highVal = data[0];
-    for (int i=0; i<size; i++){
-        if (data[i] > highVal){
-            highVal = data[i];
-            index = i;
-        }
-    }
-}
